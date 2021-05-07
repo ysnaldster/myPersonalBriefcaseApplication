4fdf6eda0ab255e0df50875dc38aea6f8375eacc
Ysnaldo Lopez <ysnaldolopez@gmail.com>
2021-05-07 16:36:36 -0500

    Se añane gh pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// Importacion componentes 
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import Footer from './components/Footer/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <React.Fragment>
+      <section className = "header">
+          <Header  name = 'Mi HV'/>
+      </section>
+      <section className="main">
+          <Main />
+      </section>
+      <section>
+          <Footer/>
+      </section>
+      </React.Fragment>
   );
 }
 
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './main.scss'
+
+class Main extends React.Component{
+    render() {
+        return (
+            <div className = "title-p">
+                <p>Hola Ysnaldo soy la seccion Main, conteniendo un p</p>   
+            </div>
+        )
+    }
+}
+
+export default Main; 
